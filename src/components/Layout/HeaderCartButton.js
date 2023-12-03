d8c424e5e2646cdda6a4a4c5ce35dca76120df86
Viraaj SIngh Gill <viraajgill2@gmail.com>
2023-12-03 19:08:13 +0530

    adding the details of cart and meals manual

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../Store/cart-context";
+import classes from "./HeaderCartButton.module.css";
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlited] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const {items} =cartCtx;
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+    
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if(cartCtx.items.length===0){
+      return
+    };
+    setBtnIsHighlited(true);
+
+    const timer =setTimeout(() => {
+      setBtnIsHighlited(false);
+    }, 300);
+
+    return ()=>{
+      clearTimeout(timer);
+    };
+
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+export default HeaderCartButton;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,49 @@
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 230,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 165,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 129,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 189,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+export default AvailableMeals;
